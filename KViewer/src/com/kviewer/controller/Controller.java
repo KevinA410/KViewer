f12157c720437eddad3d944dfa834e015d3f3d23
Kevin A <kevin.arias13m@gmail.com>
2021-03-12 09:32:01 -0600

    Test for works

--- KViewer/src/com/kviewer/controller/Controller.java
+++ KViewer/src/com/kviewer/controller/Controller.java
@@ -1,54 +1,150 @@
 package com.kviewer.controller;
 
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.io.File;
+import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import com.kviewer.view.Window;
 
 public class Controller {
+	/**
+	 * Attributes
+	 */
+	private int variable_test = 0;
+	private int index;
 	private Window win;
-	private String path;
+	private String current;
+	private File root;
+	private String[] supportedImageExtensions = {".jpeg", ".jpg", ".png", ".gif"};
+    private ArrayList<String> images = new ArrayList<String>();
 	
 	/**
 	 * Constructor
 	 * @param view
+	 * @param PATH
 	 */
-	public Controller(Window view) {
+	public Controller(Window view, String PATH) {
 		win = view;
+		current = PATH;
+		root = new File(PATH);
 		
 		win.setVisible(true);
 		
+		/**
+		 * This event change the size of image when the window is resized
+		 */
 		win.addComponentListener(new ComponentAdapter() {
 			public void componentResized(ComponentEvent e) {
-				setImage();
+				setImage(root.getAbsolutePath() + "/" + images.get(index));
+			}
+		});
+		
+		/**
+		 * This event change the current image to the next left
+		 */
+		win.left.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(index == 0)
+					index = images.size()-1;
+				else
+					index--;
+				
+				setImage(root.getAbsolutePath() + "/" + images.get(index));
+			}
+		});
+		
+		/**
+		 * This event change the current image to the next right
+		 */
+		win.right.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(index == images.size()-1)
+					index = 0;
+				else
+					index++;
+				
+				setImage(root.getAbsolutePath() + "/" + images.get(index));
 			}
 		});
 	}
+
+	/**
+	 * Initialize the application
+	 */
+	public void run() {
+		File f = new File(current);
+		
+		if(!f.exists()) {
+			win.canva.setText("We couldn't find " + f.getAbsolutePath());
+			return;
+		}
+		
+		if(!f.isDirectory()) {
+			root = f.getParentFile();
+		}
+		
+		for(String str : root.list()) {
+			if(isSupported(str))
+				images.add(str);
+		}
+		
+		index = images.indexOf(f.getName());
+		
+		if(index < 0)
+			index = 0;
+		
+		setImage(root.getAbsolutePath() + "/" + images.get(index));
+	}
 	
-	public void setImage(String path) {
-		this.path = path;
+	/**
+	 * Check if the file has a supported file format image
+	 * @param fileName
+	 * @return
+	 */
+	private boolean isSupported(String fileName) {
+		boolean isSupported = false;
+		String fileExtension = fileName.substring(fileName.lastIndexOf('.'), fileName.length());
+		
+		for(String ext : supportedImageExtensions) {
+			if(fileExtension.equals(ext)) {
+				isSupported = true;
+				break;
+			}
+		}
+		return isSupported;
+	}
+	
+	/**
+	 * Puts the image in the view
+	 * @param path
+	 */
+	private void setImage(String path) {
 		ImageIcon icon = new ImageIcon(path);
-		Image scaled = icon.getImage().getScaledInstance(win.image.getWidth(), win.image.getHeight(), Image.SCALE_SMOOTH);
-		
-		win.image.setText("");
-		win.image.setIcon(new ImageIcon(scaled));
-		
+		Image scaled = icon.getImage().getScaledInstance(win.canva.getWidth(), win.canva.getHeight(), Image.SCALE_SMOOTH);
+			
+		win.canva.setText("");
+		win.canva.setIcon(new ImageIcon(scaled));	
+		win.setTitle(Window.title + ": " + path.substring(path.lastIndexOf("/")+1, path.lastIndexOf(".")));
 	}
-	
-	private void setImage() {
-		setImage(path);
-	}
-	
+
+	/**
+	 * Main method, run the application
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		if(args.length == 0) {
-			System.out.println("You most specify the path of the image");
+			System.out.println("You have to specify a path");
 			return;
 		}
 		
 		Window win = new Window();
-		Controller con = new Controller(win);
-		
-		con.setImage(args[0]);
+		Controller con = new Controller(win, args[0]);
+		con.run();
 	}
 }
--- KViewer/src/com/kviewer/view/Window.java
+++ KViewer/src/com/kviewer/view/Window.java
@@ -1,17 +1,28 @@
 package com.kviewer.view;
 
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import java.awt.BorderLayout;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
 
 @SuppressWarnings("serial")
 public class Window extends JFrame{
 	/**
 	 * Attributes
 	 */
-	public JLabel image;
-
+	private JPanel mainPanel = (JPanel) getContentPane();
+	private JPanel buttons;
+	
+	public static String title = "KViewer";
+	public JLabel canva;
+	public JButton left;
+	public JButton right;
+	
 	/**
 	 * Create the application.
 	 */
@@ -23,13 +34,31 @@ public class Window extends JFrame{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		setBounds(100, 100, 450, 300);
+		setSize(450, 300);
+		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("KViewer");
-		getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		image = new JLabel("The image will load here...", SwingConstants.CENTER);
-		getContentPane().add(image, BorderLayout.CENTER);
+		buttons = new JPanel();
+		mainPanel.setLayout(new BorderLayout(0, 0));
+		buttons.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		mainPanel.add(buttons, BorderLayout.SOUTH);
+		
+		canva = new JLabel("The image will load here...", SwingConstants.CENTER);
+		mainPanel.add(canva);
+		
+		left = new JButton("<");
+		buttons.add(left);
+		
+		right = new JButton(">");
+		buttons.add(right);
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+			System.out.println("Unsupported look and feel GUI");
+		}
 	}
-
 }
